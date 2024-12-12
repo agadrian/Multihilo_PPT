5fb83e874ee42789e0f0602c53184ae0a8805e09
agadrian <arroyogomezadrian@gmail.com>
2024-12-12 19:13:04 +0100

    16 jugadores y 16 hilos, se enfrentan de dos en dos en varias rondas.

--- PiedraPapelTijera_MultiHilo/Program.cs
+++ PiedraPapelTijera_MultiHilo/Program.cs
@@ -3,47 +3,46 @@ using System.Threading;
 
 public class PiedraPapelTijera_MultiHilo
 {
-    private static string j1Eleccion;
-    private static string j2Eleccion;
+    // Guadar las elecciones
+    private static string[] elecciones = new string[16];
+    private static Random random = new Random();
+    
     private static object bloqueo = new object();
 
-    private static Random random = new Random();
+    
     
     public static void Main()
     {
+        // Crear los 16 hilos
+        Thread[] hilos = new Thread[16];
+
         
-        // Creamos los dos hilos, uno para cada jugador
-        Thread jugador1 = new Thread(Jugador1);
-        Thread jugador2 = new Thread(Jugador2);
+        // Crear los 16 jugadores, 1 en cada hilo
+        for (int i = 0; i < 16; i++)
+        {
+            
+            int jugador = i;
+            hilos[i] = new Thread((() => Jugador(jugador)));
+            hilos[i].Start();
+        }
         
-        // Inicia los hilos de ambos jugadores
-        jugador1.Start();
-        jugador2.Start();
-        
-        // Permite que el hilo principal, espere a que estos dos acaben antes de seguir,, para que cuando vaya a comprobar la victoria ambos hayn elegido ya.
-        jugador1.Join();
-        jugador2.Join();
-        
-        
-        // Comprueba los resultados
-        CheckVictoria();
+        // Esperamos a que todos los hilos terminen su ejecución
+        for (int i = 0; i < 16; i++)
+        {
+            hilos[i].Join();
+        }
+
+
+        IniciarPartidas();
+       
 
     }
     
-    
-    static void Jugador1()
+    static void Jugador(int jugador)
     {
-        // Simulamos la elección del jugador 1
-        j1Eleccion = GenerarEleccion();
-        Console.WriteLine("Jugador 1 elige: " + j1Eleccion);
-    }
-
-    private static void Jugador2()
-    {
-        
-        // Simulamos la elección del jugador 2
-        j2Eleccion = GenerarEleccion();
-        Console.WriteLine("Jugador 2 elige: " + j2Eleccion);
+        // Cada jugador elige aleatoriamente Piedra, Papel o Tijera
+        elecciones[jugador] = GenerarEleccion();
+        Console.WriteLine($"Jugador {jugador + 1} elige: {elecciones[jugador]}");
     }
     
     
@@ -53,25 +52,75 @@ public class PiedraPapelTijera_MultiHilo
         string[] opciones = { "Piedra", "Papel", "Tijera" };
         return opciones[random.Next(0, opciones.Length)]; // 0-2
     }
-    
-    private static void CheckVictoria()
+
+
+
+    private static void IniciarPartidas()
     {
-        Console.WriteLine("Resultados de la partida: \n");
-        if (j1Eleccion == j2Eleccion)
+        int[] ganadores = new int[16];
+
+        // Meter todos los indices al empezar en los ganadores
+        for (int i = 0; i < 16; i++)
         {
-            Console.WriteLine("Empate!");
+            ganadores[i] = i;
+        }
+
+        
+        while (ganadores.Length > 1)
+        {
+            // La mitad de los jugadores que quedan despues de la primera ronda. Se igauala a la lista ganadores una vez acabe el turno de partidas.
+            int[] siguienteRonda = new int[ganadores.Length / 2];
+
+            
+            for (int i = 0; i < ganadores.Length / 2; i++)
+            {
+                // Vamos haciendo pasrtidas de dos en dos. El primero con el segundo, tercero coin el cuarto, y asi sucesivamente
+                int jugador1 = ganadores[i * 2];
+                int jugador2 = ganadores[i * 2 + 1];
+                Console.WriteLine($"Jugador {jugador1 + 1} contra {jugador2 + 1}");
+
+                // Vamos añadiendo el ganador a la lista de siguienteronda
+                siguienteRonda[i] = CheckVictoria(jugador1, jugador2);
+            }
+            
+            
+            // Movemos los nuevos finalistas a la lista de ganadores inicial.
+            ganadores = siguienteRonda;
+
+            if (ganadores.Length > 1)
+            {
+                Console.WriteLine($"\nSiguiente ronda Comienza... Jugadores restantes: {string.Join(", ", ganadores.Select(j => j + 1))}");
+            }
+            
+        }
+        
+        Console.WriteLine($"\nEl jugador final y ganador es: {ganadores[0] + 1}");
+       
+    }
+    
+    private static int CheckVictoria(int jug1, int jug2)
+    {
+        //Console.WriteLine("Resultados de la partida: \n");
+        if (elecciones[jug1] == elecciones[jug2])
+        {
+            Console.WriteLine($"Empate! Pasa el juagador: {jug1+1}");
+            return jug1; // Seria empate, pero devuelvo el jug1 por ejemplo
         }
         else if (
-            (j1Eleccion == "Piedra" && j2Eleccion == "Tijera") ||
-            (j1Eleccion == "Papel" && j2Eleccion == "Piedra") ||
-            (j1Eleccion == "Tijera" && j2Eleccion == "Papel")
+            (elecciones[jug1] == "Piedra" && elecciones[jug2] == "Tijera") ||
+            (elecciones[jug1] == "Papel" && elecciones[jug2] == "Piedra") ||
+            (elecciones[jug1] == "Tijera" && elecciones[jug2] == "Papel")
         )
         {
-            Console.WriteLine("Jugador 1 gana!");
+            Console.WriteLine($"Jugador {jug1+1} gana!");
+            return jug1;
+            
         }
         else
         {
-            Console.WriteLine("Jugador 2 gana!");
+            Console.WriteLine($"Jugador {jug2+1} gana!");
+            return jug2;
+           
         }
     }
 }
